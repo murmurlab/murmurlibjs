b28ff6bfbda3b147985ab24b75c38be6312d347d
Ubuntu <murmur@freevm.rlqz2nglbvlu3bxuoofla1yd4b.bx.internal.cloudapp.net>
2023-05-11 02:17:44 +0000

    freevm

--- https_server.js
+++ https_server.js
@@ -8,20 +8,25 @@ try
 	console.error("https support is disabled!");
 }
 
-import {fs} from "node:fs";
+import * as fs from "node:fs";
 
-const	
-	options = 
-	{
-		key: fs.readFileSync("test/fixtures/keys/agent2-key.pem"),
-		cert: fs.readFileSync("test/fixtures/keys/agent2-cert.pem"),
-	},
+const
 	requestListener = (req, res) =>
 	{
 		res.write("Hello World!"); //write a response to the client
 		res.end(); //end the response
 	},
-	server = https.createServer(options , requestListener);
+	config = (cfg) =>
+	{
+		const
+			server = https.createServer(options , requestListener),
+			options = 
+			{
+				key: fs.readFileSync(cfg.key),
+				cert: fs.readFileSync(cfg.cert),
+			}
+		server
+			.listen(80, "10.0.0.4"); //the server object listens on port 8080	
+	}
 
-server
-	.listen(80, "10.0.0.4"); //the server object listens on port 8080
+export { config }
